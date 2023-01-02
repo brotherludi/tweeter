068595933cd2a6e159be5b195fd27e91759ee87e
Di Lu <brotherludi@hotmail.com>
2023-01-02 15:27:57 -0700

    Renders the tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,68 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further, it is by standing on the shoulders of giants."
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function (tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      console.log($tweet);
+      $('#tweets-container').append($tweet);
+    }
+  }
+
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`
+  <article>
+        <header class="tweet-header">
+          <div class="user-profile">
+            <img alt="User Avatar" class="user-icon" src="${tweet.user.avatars}"></img> 
+            <h4 class="user-name">${tweet.user.name}</h4>
+          </div>
+          <div>
+            <h4 class="user-handle">${tweet.user.handle}</h4>
+          </div>
+        </header>
+        <div class="tweet-text">
+          ${tweet.content.text}
+        </div>
+        <footer class="tweet-footer">
+          <span class="tweet-date">${timeago.format(tweet.created_at)}</span>
+          <div class="tweet-response">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>`);
+    return $tweet;
+  }
+
+  renderTweets(data);
+})
