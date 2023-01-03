c3d5ae2b0ea35eb8c3d8019e85ef1fc53b49fe43
Di Lu <brotherludi@hotmail.com>
2023-01-02 18:18:38 -0700

    Fetches tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,36 +5,10 @@
  */
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further, it is by standing on the shoulders of giants."
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   $("#new-tweet-form").submit(function (event) {
     event.preventDefault();
-    const tweet = $(this).serialize();
-    $.post("/tweets/", tweet);
+    const newTweet = $(this).serialize();
+    $.post("/tweets/", newTweet);
   })
 
   const renderTweets = function (tweets) {
@@ -43,6 +17,14 @@ $(document).ready(function () {
       $('#tweets-container').append($tweet);
     }
   }
+
+  const loadTweets = function () {
+    $.get("/tweets/", function (newTweet) {
+      renderTweets(newTweet);
+    });
+  }
+
+  loadTweets();
 
   const createTweetElement = function (tweet) {
     let $tweet = $(`
@@ -70,6 +52,4 @@ $(document).ready(function () {
       </article>`);
     return $tweet;
   }
-
-  renderTweets(data);
 })
